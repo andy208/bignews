9e04755d9e3283c0a32b130150fc0344483baa4e
andy208 <1029123033@qq.com>
2020-05-16 11:41:49 +0800

    03使用第二种方式实现登录功能

--- admin/js/login.js
+++ admin/js/login.js
@@ -1,23 +1,26 @@
 $(function () {
-    $('.input_sub').on('click', function (e) {
+    //给form表单注册submit事件
+    $('.login_form').on('submit', function (e) {
         e.preventDefault()
-        var username = $('.input_txt').val()
-        var password = $('.input_pass').val()
-        if ($.trim(username) == '' || $.trim(password) == '') {
-            alert('用户名或密码不能为空，请重新输入')
-            return;
-        }
         $.ajax({
             type: 'post',
             url: 'http://localhost:8080/api/v1/admin/user/login',
-            data: {
-                username: username,
-                password: password,
+            data: $(this).serialize(),
+            beforeSend: function () {
+                var flag = false
+                $('.login_form input[name]').each(function (index, ele) {
+                    if (!$.trim($(ele).val())) {
+                        flag = true
+                    }
+                })
+                if (flag) {
+                    alert('输入的用户名或密码不能为空')
+                    return false
+                }
             },
             success: function (res) {
-                console.log(res)
                 if (res.code == 200) {
-                    alert('登录成功..')
+                    alert('登录成功')
                     window.location.href = './index.html'
                 } else {
                     alert(res.msg)
